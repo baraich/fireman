c75aaef33c18fdde4f76462572d21c40df1cf853
Baraich <gurvirsingh.baraich@gmail.com>
2025-07-16 09:30:24 +0530

    code-view added

---
+++ src/components/code-view/index.tsx
@@ -0,0 +1,27 @@
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-typescript";
+import "./code-theme.css";
+import { useEffect } from "react";
+
+interface Props {
+  code: string;
+  lang: string;
+}
+
+export default function CodeView({ code, lang }: Props) {
+  useEffect(
+    function () {
+      Prism.highlightAll();
+    },
+    [code]
+  );
+
+  return (
+    <pre className="p-2 bg-transparent border-none rounded-none m-0 text-xs">
+      <code className={`language-${lang}`}>{code}</code>
+    </pre>
+  );
+}
---
+++ src/components/tree-view.tsx
@@ -0,0 +1,118 @@
+import { TreeItem } from "@/types";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarProvider,
+  SidebarRail,
+} from "./ui/sidebar";
+import { ChevronRightIcon, FileIcon, FolderIcon } from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+
+interface TreeViewProps {
+  data: TreeItem[];
+  value?: string | null;
+  onSelect?: (filePath: string) => void;
+}
+
+export default function TreeView({
+  data,
+  onSelect,
+  value,
+}: TreeViewProps) {
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="none" className="w-full">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {data.map((item, idx) => (
+                  <Tree
+                    key={idx}
+                    item={item}
+                    selectedValue={value}
+                    onSelect={onSelect}
+                    parentPath=""
+                  />
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarRail />
+      </Sidebar>
+    </SidebarProvider>
+  );
+}
+
+interface TreeProps {
+  item: TreeItem;
+  selectedValue?: string | null;
+  onSelect?: (value: string) => void;
+  parentPath: string;
+}
+const Tree = ({
+  item,
+  parentPath,
+  onSelect,
+  selectedValue,
+}: TreeProps) => {
+  const [name, ...items] = Array.isArray(item) ? item : [item];
+  const currentPath = parentPath ? `${parentPath}/${name}` : name;
+
+  // File
+  if (!items.length) {
+    const isSelected = selectedValue === currentPath;
+    return (
+      <SidebarMenuButton
+        isActive={isSelected}
+        className="data-[active=true]:bg-transparent"
+        onClick={() => onSelect?.(currentPath)}
+      >
+        <FileIcon />
+        <span className="truncate">{name}</span>
+      </SidebarMenuButton>
+    );
+  }
+
+  // Folder
+  return (
+    <SidebarMenuItem>
+      <Collapsible
+        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+        defaultOpen
+      >
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <ChevronRightIcon className="transition-transform" />
+            <FolderIcon />
+            <span className="truncate">{name}</span>
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub className="mx-0 ml-3.5 px-0.5 pl-2.5">
+            {items.map((subItem, idx) => (
+              <Tree
+                key={idx}
+                item={subItem}
+                selectedValue={selectedValue}
+                onSelect={onSelect}
+                parentPath={currentPath}
+              />
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+    </SidebarMenuItem>
+  );
+};
